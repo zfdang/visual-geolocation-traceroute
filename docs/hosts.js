9ca4d51640d3d90b6a89f0cad0d5f4d025c23296
dantifer <dantifer@gmail.com>
2022-03-28 21:41:50 +0800

    update docs

---
+++ docs/hosts.js
@@ -0,0 +1,17 @@
+let hosts = [
+
+{"n": 1, "ip":"221.218.160.1", "desc": "221.218.160.1 (China Unicom Beijing Province Network; Haidian; Beijing; China)", "geo": {"lat": 39.9881, "lng": 116.28464}},
+{"n": 2, "ip":"61.48.27.5", "desc": "61.48.27.5 (China Unicom Beijing Province Network; Beijing; Beijing; China)", "geo": {"lat": 39.9075, "lng": 116.39724}},
+{"n": 3, "ip":"124.65.62.149", "desc": "124.65.62.149 (China Unicom Beijing Province Network; Beijing; Beijing; China)", "geo": {"lat": 39.9075, "lng": 116.39724}},
+{"n": 4, "ip":"202.96.12.181", "desc": "202.96.12.181 (Unicom BJ; China Unicom Beijing Province Network; Beijing; Beijing; China)", "geo": {"lat": 39.9075, "lng": 116.39724}},
+{"n": 5, "ip":"219.158.9.202", "desc": "219.158.9.202 (China Unicom China169 Network; Jinrongjie; Beijing; China)", "geo": {"lat": 39.9174, "lng": 116.3614}},
+{"n": 6, "ip":"219.158.3.182", "desc": "219.158.3.182 (China Unicom China169 Network; Jinrongjie; Beijing; China)", "geo": {"lat": 39.9174, "lng": 116.3614}},
+{"n": 7, "ip":"219.158.96.38", "desc": "219.158.96.38 (CNC group; China Unicom China169 Network; Guangzhou; Guangdong; China)", "geo": {"lat": 23.1291, "lng": 113.2644}},
+{"n": 8, "ip":"12.246.229.121", "desc": "12.246.229.121 (SADA-PRTD Pool-IN6RAS9A; AT&T Services, Inc.; Longview; Texas; United States)", "geo": {"lat": 32.5298, "lng": -94.79494}},
+{"n": 9, "ip":"12.123.242.22", "desc": "12.123.242.22 (AT&T Worldnet Services; AT&T Services, Inc.; New York; New York; United States)", "geo": {"lat": 40.7128, "lng": -74.0064}},
+{"n": 10, "ip":"12.122.163.37", "desc": "12.122.163.37 (AT&T Worldnet Services; AT&T Services, Inc.; New York; New York; United States)", "geo": {"lat": 40.7128, "lng": -74.0064}},
+{"n": 11, "ip":"12.122.1.90", "desc": "12.122.1.90 (AT&T Worldnet Services; AT&T Services, Inc.; Chicago; Illinois; United States)", "geo": {"lat": 41.8781, "lng": -87.62984}},
+{"n": 12, "ip":"12.123.206.249", "desc": "12.123.206.249 (AT&T Worldnet Services; AT&T Services, Inc.; Chicago; Illinois; United States)", "geo": {"lat": 41.8781, "lng": -87.62984}},
+{"n": 13, "ip":"140.91.194.89", "desc": "140.91.194.89 (Oracle Public Cloud; Oracle Corporation; Phoenix; Arizona; United States)", "geo": {"lat": 33.4656, "lng": -111.99564}},
+{"n": 14, "ip":"129.153.75.169", "desc": "129.153.75.169 (Oracle Cloud Infrastructure (us-phoenix-1); Oracle Corporation; Phoenix; Arizona; United States)", "geo": {"lat": 33.4656, "lng": -111.99564}},
+];
