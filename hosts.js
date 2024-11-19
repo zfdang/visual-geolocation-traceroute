e9bd151d248d87de3b700e3662c6130f597c62b4
dantifer <dantifer@gmail.com>
2024-11-19 13:34:01 +0800

    show hosts in the page

--- hosts.js
+++ hosts.js
@@ -1,13 +1,13 @@
 let hosts = [
 
 
-{"n": 2, "ip":"221.183.89.97", "desc": "221.183.89.97 (China Mobile; China Mobile communications corporation; Guangzhou; Guangdong; China)", "geo": {"lat": 23.1317, "lng": 113.2664}},
-{"n": 3, "ip":"221.183.46.249", "desc": "221.183.46.249 (China Mobile; China Mobile communications corporation; Guangzhou; Guangdong; China)", "geo": {"lat": 23.1317, "lng": 113.2664}},
-{"n": 4, "ip":"223.120.13.49", "desc": "223.120.13.49 (China Mobile International Limited; China Mobile International Limited; Kwai Chung; Kwai Tsing; Hong Kong)", "geo": {"lat": 22.3616, "lng": 114.1334}},
-{"n": 5, "ip":"223.120.6.18", "desc": "223.120.6.18 (China Mobile International Limited; China Mobile International Limited; Kwai Chung; Kwai Tsing; Hong Kong)", "geo": {"lat": 22.3616, "lng": 114.1334}},
-{"n": 6, "ip":"207.45.219.130", "desc": "207.45.219.130 (TATA COMMUNICATIONS (AMERICA) INC; TATA COMMUNICATIONS (AMERICA) INC; Los Angeles; California; United States)", "geo": {"lat": 34.0549, "lng": -118.2434}},
-{"n": 7, "ip":"207.45.219.129", "desc": "207.45.219.129 (TATA COMMUNICATIONS (AMERICA) INC; TATA COMMUNICATIONS (AMERICA) INC; Los Angeles; California; United States)", "geo": {"lat": 34.0549, "lng": -118.2434}},
-{"n": 8, "ip":"64.86.252.59", "desc": "64.86.252.59 (Tata Communications, Ltd.; TATA COMMUNICATIONS (AMERICA) INC; Los Angeles; California; United States)", "geo": {"lat": 34.0479, "lng": -118.2554}},
-{"n": 9, "ip":"180.87.151.31", "desc": "180.87.151.31 (Tata Communications (CANADA) Ltd; TATA COMMUNICATIONS (AMERICA) INC; Sydney; New South Wales; Australia)", "geo": {"lat": -33.8688, "lng": 151.2094}},
-{"n": 10, "ip":"146.56.131.219", "desc": "146.56.131.219 (Oracle Cloud Infrastructure (ap-seoul-1); Oracle Corporation; Seoul; Seoul; South Korea)", "geo": {"lat": 37.5985, "lng": 126.97834}},
+{"n": 2, "ip":"211.136.61.141", "desc": "211.136.61.141 (China Mobile Communications Corporation - beijing company; China Mobile communications corporation; Jinrongjie; Beijing; China)", "geo": {"lat": 39.9155, "lng": 116.364}},
+{"n": 3, "ip":"221.183.89.49", "desc": "221.183.89.49 (China Mobile; China Mobile communications corporation; Guangzhou; Guangdong; China)", "geo": {"lat": 23.1317, "lng": 113.2664}},
+{"n": 4, "ip":"221.183.89.70", "desc": "221.183.89.70 (China Mobile; China Mobile communications corporation; Guangzhou; Guangdong; China)", "geo": {"lat": 23.1317, "lng": 113.2664}},
+{"n": 5, "ip":"221.183.89.173", "desc": "221.183.89.173 (China Mobile; China Mobile communications corporation; Guangzhou; Guangdong; China)", "geo": {"lat": 23.1317, "lng": 113.2664}},
+{"n": 6, "ip":"223.120.14.193", "desc": "223.120.14.193 (China Mobile International Limited; China Mobile International Limited; Kwai Chung; Kwai Tsing; Hong Kong)", "geo": {"lat": 22.3616, "lng": 114.1334}},
+{"n": 7, "ip":"223.120.3.114", "desc": "223.120.3.114 (China Mobile International Limited; China Mobile International Limited; Kwai Chung; Kwai Tsing; Hong Kong)", "geo": {"lat": 22.3616, "lng": 114.1334}},
+{"n": 8, "ip":"223.119.19.154", "desc": "223.119.19.154 (cmi; China Mobile International Limited; Kwai Chung; Kwai Tsing; Hong Kong)", "geo": {"lat": 22.3616, "lng": 114.1334}},
+{"n": 9, "ip":"140.91.214.13", "desc": "140.91.214.13 (Oracle Corporation; Oracle Corporation; Seoul; Seoul; South Korea)", "geo": {"lat": 37.545, "lng": 126.80784}},
+{"n": 10, "ip":"132.145.93.75", "desc": "132.145.93.75 (Oracle Cloud Infrastructure (ap-seoul-1); Oracle Corporation; Seoul; Seoul; South Korea)", "geo": {"lat": 37.5985, "lng": 126.97834}},
 ];
